75cf272c0e2ba8145b8546cf53c05db88acba097
Vinffs <vin.demiranda@gmail.com>
2023-12-19 14:32:48 +0100

    added buttons to comic cards

--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -2,13 +2,14 @@
     <div class="row container justify-content-center">
         @foreach (config('comics.comics') as $comic)
             <div class="col-5 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-2">
-                <div class="card py-3 border-0">
+                <div class="card border-white my-3 d-flex flex-column justify-content-between">
                     <div>
                         <img src="{{ $comic['thumb'] }}" class="card-img-top rounded-0" alt={{ $comic['series'] }}>
                     </div>
-                    <div class=" card-body d-flex p-0">
-                        <h5>{{ $comic['series'] }}</h5>
+                    <div class=" card-body d-flex p-0 pt-2 mx-auto">
+                        <h5 class="text-center">{{ $comic['series'] }}</h5>
                     </div>
+                    <a class="btn-primary btn" href="">See Details</a>
                 </div>
             </div>
         @endforeach
