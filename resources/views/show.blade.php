d77e728658e5696829c3777e5fb9c3e269bbd02e
Vinffs <vin.demiranda@gmail.com>
2023-12-19 15:58:59 +0100

    added graphic details to show page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,15 +11,27 @@
                 <img class="imagePoster" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
             </div>
         </div>
-        <section id="comicInfo" class="container">
+        <section id="comicInfo" class="container py-5">
             <div class="row">
-                <div class="col-9 p-0">
-                    <h2>{{ $comic['title'] }}</h2>
-                    <p>{{ $comic['price'] }}</p>
-                    <p>{{ $comic['description'] }}</p>
+                <div class="col-9 p-0 pe-5">
+                    <h2 class="pb-3">{{ $comic['title'] }}</h2>
+                    <div id="bar" class="d-flex">
+                        <div class="col-9 d-flex justify-content-between px-4 py-2">
+                            <span>U.S. Price: {{ $comic['price'] }}</span>
+                            <span>AVAILABLE</span>
+                        </div>
+                        <div class="col-3 text-center availableBox py-2">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Check Availability
+                            </a>
+                        </div>
+                    </div>
+                    <p class="descriptionComic">{{ $comic['description'] }}</p>
                 </div>
-                <div class="col-3">
-                    QUESTO E' LO SPAZIO PER L'AD
+                <div class="col-3 p-0 text-end">
+                    <p class="adv">ADVERTISEMENT</p>
+                    <img class="" src="{{ Vite::asset('resources/images/adv.jpg') }}" alt="advertisement">
                 </div>
             </div>
         </section>
